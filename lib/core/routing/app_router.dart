b1b071967da6f91416a6914ae872a12e1d5227f2
amrElzohairy <aelzohairy9@gmail.com>
2025-05-12 18:56:24 +0300

    provide sign in cubit to sign in view

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,9 @@
+import 'package:cars_store/core/helpers/service_locator.dart';
+import 'package:cars_store/feature/auth/domain/repo/auth_repo.dart';
+import 'package:cars_store/feature/auth/presentation/cubits/cubit/sign_in_cubit.dart';
 import 'package:cars_store/feature/auth/presentation/views/login_view.dart';
 import 'package:cars_store/feature/auth/presentation/views/sign_up_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -9,7 +13,11 @@ abstract class AppRouter {
       GoRoute(
         path: LoginView.routeName,
         name: LoginView.routeName,
-        builder: (context, state) => const LoginView(),
+        builder:
+            (context, state) => BlocProvider(
+              create: (context) => SignInCubit(authRepo: getIt<AuthRepo>()),
+              child: const LoginView(),
+            ),
       ),
       GoRoute(
         path: SignUpView.routeName,
