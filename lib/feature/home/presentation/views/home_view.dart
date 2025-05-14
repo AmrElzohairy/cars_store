91e3a783d28f2b653d946d8eeb5a6d2195347657
amrElzohairy <aelzohairy9@gmail.com>
2025-05-14 19:00:04 +0300

    add home drawer and app bar

--- lib/feature/home/presentation/views/home_view.dart
+++ lib/feature/home/presentation/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:cars_store/core/utils/app_colors.dart';
+import 'package:cars_store/core/utils/spacing_widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +7,20 @@ class HomeView extends StatelessWidget {
   static const routeName = '/home_view';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home View')));
+    return Scaffold(
+      drawer: const Drawer(),
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Spacer(),
+            HorizontalSpace(width: 10),
+            Text('CarsStore', style: TextStyle(color: AppColors.primaryColor)),
+            Spacer(flex: 2),
+            Icon(Icons.notifications_none),
+          ],
+        ),
+      ),
+      body: const Center(child: Text('Home View')),
+    );
   }
 }
