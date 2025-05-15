713043f3f15b6b47a9cf923fd8a8d025e9d57670
amrElzohairy <aelzohairy9@gmail.com>
2025-05-15 20:50:18 +0300

    add featured cars entity

---
+++ lib/feature/home/domain/entity/home_featured_list_entity.dart
@@ -0,0 +1,11 @@
+class HomeFeaturedListEntity {
+  final String id;
+  final String name;
+  final String image;
+
+  HomeFeaturedListEntity({
+    required this.id,
+    required this.name,
+    required this.image,
+  });
+}
