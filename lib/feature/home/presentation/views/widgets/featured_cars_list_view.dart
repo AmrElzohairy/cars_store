1b607580b28afa5dc8d26b2495907b007644c2b6
amrElzohairy <aelzohairy9@gmail.com>
2025-05-15 20:03:31 +0300

    refacto cars list hight

--- lib/feature/home/presentation/views/widgets/featured_cars_list_view.dart
+++ lib/feature/home/presentation/views/widgets/featured_cars_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:cars_store/core/utils/media_query_size.dart';
 import 'package:cars_store/feature/home/presentation/views/widgets/featured_cars_list_item.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,7 @@ class FeaturedCarsListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 180,
+      height: height(context) * 0.2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: 10,
