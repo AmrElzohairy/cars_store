4689f3a9396e36e9d50c31eb042a83390d0c9180
amrElzohairy <aelzohairy9@gmail.com>
2025-05-16 16:42:50 +0300

    add Recommended Cars Entity

--- lib/feature/home/domain/entity/recommended_cars_entity.dart
+++ lib/feature/home/domain/entity/recommended_cars_entity.dart
@@ -1,27 +1,31 @@
 import 'package:cars_store/feature/home/data/models/featured_cars_model/featured_cars_model.dart';
 import 'package:equatable/equatable.dart';
 
-class HomeFeaturedListEntity extends Equatable {
+class RecommendedCarsEntity extends Equatable {
   final String id;
   final String name;
   final List<dynamic> images;
+  final String description;
+  final num price;
 
-  const HomeFeaturedListEntity({
+  const RecommendedCarsEntity({
     required this.id,
     required this.name,
     required this.images,
+    required this.description,
+    required this.price,
   });
 
   @override
   List<Object?> get props => [id, name, images];
 
-  factory HomeFeaturedListEntity.fromFeaturedCarsModel(
-    FeaturedCarsModel model,
-  ) {
-    return HomeFeaturedListEntity(
+  factory RecommendedCarsEntity.fromFeaturedCarsModel(FeaturedCarsModel model) {
+    return RecommendedCarsEntity(
       id: model.carId ?? '',
       name: model.modelName ?? '',
       images: model.imagesUrls ?? [],
+      description: model.description ?? '',
+      price: model.price ?? 0,
     );
   }
 }
