62e67af23d0ff75d850d42f46cdd24929293445d
amrElzohairy <aelzohairy9@gmail.com>
2025-05-16 14:01:59 +0300

    add mapper to featured cars

--- lib/feature/home/domain/entity/home_featured_list_entity.dart
+++ lib/feature/home/domain/entity/home_featured_list_entity.dart
@@ -1,11 +1,27 @@
-class HomeFeaturedListEntity {
+import 'package:cars_store/feature/home/data/models/featured_cars_model.dart';
+import 'package:equatable/equatable.dart';
+
+class HomeFeaturedListEntity extends Equatable {
   final String id;
   final String name;
-  final String image;
+  final List<String> images;
 
-  HomeFeaturedListEntity({
+  const HomeFeaturedListEntity({
     required this.id,
     required this.name,
-    required this.image,
+    required this.images,
   });
+
+  @override
+  List<Object?> get props => [id, name, images];
+
+  factory HomeFeaturedListEntity.fromFeaturedCarsModel(
+    FeaturedCarsModel model,
+  ) {
+    return HomeFeaturedListEntity(
+      id: model.modelId!,
+      name: model.modelName!,
+      images: model.imagesUrls!,
+    );
+  }
 }
