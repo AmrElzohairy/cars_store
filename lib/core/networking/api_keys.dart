c0a578e77af1eaeef36dee2f2c591f13f9cb88e2
amrElzohairy <aelzohairy9@gmail.com>
2025-05-18 18:44:45 +0300

    add toggle Favorite key

--- lib/core/networking/api_keys.dart
+++ lib/core/networking/api_keys.dart
@@ -3,5 +3,6 @@ class ApiKeys {
   static const String signIn = "Auth/SignIn";
   static const String signUp = "Auth/SignUp";
   static const String featuredCars = "Car/Featured";
-  static const String recommendedCars = "Car/Recommended";
+  static const String recommendedCars = "Car/Recommended"; 
+  static const String toggleFavorite = "Favorite/ToggleFavorite";
 }
