d6af46699ee02ef1a39e75fd540d9b05aba13576
amrElzohairy <aelzohairy9@gmail.com>
2025-05-20 16:42:03 +0300

    add logic if token ended

--- lib/core/networking/auth_interceptor.dart
+++ lib/core/networking/auth_interceptor.dart
@@ -1,10 +1,11 @@
 import 'package:cars_store/core/cache/cache_constants.dart';
 import 'package:cars_store/core/cache/cache_helper.dart';
+import 'package:cars_store/core/routing/app_router.dart';
+import 'package:cars_store/feature/auth/presentation/views/login_view.dart';
 import 'package:dio/dio.dart';
 
 class AuthInterceptor extends Interceptor {
   AuthInterceptor();
-
   @override
   Future<void> onRequest(
     RequestOptions options,
@@ -16,5 +17,20 @@ class AuthInterceptor extends Interceptor {
       options.headers['Authorization'] = 'Bearer $token';
     }
     return handler.next(options);
+  }
+
+  @override
+  Future<void> onError(
+    DioException err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    if (err.response?.statusCode == 401) {
+      AppRouter.router.goNamed(LoginView.routeName);
+    }
+    if (err.response?.statusCode == 403) {
+      AppRouter.router.goNamed(LoginView.routeName);
+    }
+
+    return handler.next(err);
   }
 }
