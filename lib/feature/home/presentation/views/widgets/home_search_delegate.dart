c6805981d8119eeb254072a108540d808670152f
amrElzohairy <aelzohairy9@gmail.com>
2025-05-21 21:05:56 +0300

    add search feature

--- lib/feature/home/presentation/views/widgets/home_search_delegate.dart
+++ lib/feature/home/presentation/views/widgets/home_search_delegate.dart
@@ -1,12 +1,20 @@
+import 'package:cars_store/feature/home/data/models/search_model/search_model.dart';
+import 'package:cars_store/feature/home/presentation/cubits/search_cubit/search_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeSearchDelegate extends SearchDelegate {
+  final SearchCubit searchCubit;
+
+  HomeSearchDelegate({required this.searchCubit});
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
         onPressed: () {
           query = '';
+          searchCubit.searchCars('');
         },
         icon: const Icon(Icons.clear),
       ),
@@ -25,27 +33,132 @@ class HomeSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: Center(child: Text('Search result for "$query"')),
+    if (query.isEmpty) {
+      return _buildEmptySearchView();
+    }
+
+    searchCubit.searchCars(query);
+
+    return BlocBuilder<SearchCubit, SearchState>(
+      bloc: searchCubit,
+      builder: (context, state) {
+        if (state is SearchLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is SearchError) {
+          return Center(child: Text('Error: ${state.errorMessage}'));
+        } else if (state is SearchSuccess) {
+          if (state.searchResults.isEmpty) {
+            return Center(child: Text('No results found for "$query"'));
+          }
+          return _buildSearchResultsList(state.searchResults);
+        }
+        return _buildEmptySearchView();
+      },
     );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container(
+    if (query.isEmpty) {
+      return _buildEmptySearchView();
+    }
+
+    // Perform search as user types
+    searchCubit.searchCars(query);
+
+    return BlocBuilder<SearchCubit, SearchState>(
+      bloc: searchCubit,
+      builder: (context, state) {
+        if (state is SearchLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is SearchSuccess) {
+          if (state.searchResults.isEmpty) {
+            return Center(child: Text('No results found for "$query"'));
+          }
+          return _buildSearchResultsList(state.searchResults);
+        }
+        return _buildEmptySearchView();
+      },
+    );
+  }
+
+  Widget _buildSearchResultsList(List<SearchModel> searchResults) {
+    return ListView.builder(
+      itemCount: searchResults.length,
       padding: const EdgeInsets.all(16),
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Suggestion $index'),
+      itemBuilder: (context, index) {
+        final car = searchResults[index];
+        return Card(
+          elevation: 2,
+          margin: const EdgeInsets.only(bottom: 12),
+          child: ListTile(
+            contentPadding: const EdgeInsets.all(8),
+            leading:
+                car.imagesUrls != null && car.imagesUrls!.isNotEmpty
+                    ? ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        car.imagesUrls![0].toString(),
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                        errorBuilder:
+                            (context, error, stackTrace) => Container(
+                              width: 80,
+                              height: 80,
+                              color: Colors.grey[300],
+                              child: const Icon(
+                                Icons.car_repair,
+                                color: Colors.grey,
+                              ),
+                            ),
+                      ),
+                    )
+                    : Container(
+                      width: 80,
+                      height: 80,
+                      color: Colors.grey[300],
+                      child: const Icon(Icons.car_repair, color: Colors.grey),
+                    ),
+            title: Text(
+              '${car.carBrand ?? ''} ${car.modelName ?? ''}',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (car.yearOfProduction != null)
+                  Text('Year: ${car.yearOfProduction}'),
+                if (car.price != null)
+                  Text(
+                    'Price: \$${car.price}',
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                  ),
+              ],
+            ),
             onTap: () {
-              query = 'Suggestion $index';
-              showResults(context);
+              // Navigate to car details page
+              // You can pass the car ID to the details page
+              close(context, car.carId);
             },
-          );
-        },
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildEmptySearchView() {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.search, size: 64, color: Colors.grey),
+          SizedBox(height: 16),
+          Text(
+            'Search for cars by brand, model, or year',
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+        ],
       ),
     );
   }
--- lib/feature/home/presentation/views/widgets/home_search_field_and_filters.dart
+++ lib/feature/home/presentation/views/widgets/home_search_field_and_filters.dart
@@ -1,8 +1,10 @@
 import 'package:cars_store/core/utils/assets.dart';
 import 'package:cars_store/core/utils/spacing_widgets.dart';
 import 'package:cars_store/core/widgets/custom_search_field.dart';
+import 'package:cars_store/feature/home/presentation/cubits/search_cubit/search_cubit.dart';
 import 'package:cars_store/feature/home/presentation/views/widgets/home_search_delegate.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 class HomeSearchFieldAndFilters extends StatelessWidget {
@@ -14,7 +16,20 @@ class HomeSearchFieldAndFilters extends StatelessWidget {
       children: [
         CustomSearchField(
           onTap: () {
-            showSearch(context: context, delegate: HomeSearchDelegate());
+            // Get the SearchCubit from the nearest context
+            final searchCubit = context.read<SearchCubit>();
+            
+            // Show search with our delegate
+            showSearch(
+              context: context, 
+              delegate: HomeSearchDelegate(searchCubit: searchCubit),
+            ).then((carId) {
+              // Handle navigation to car details if a car was selected
+              if (carId != null) {
+                // Navigate to car details page using the carId
+                // Example: Navigator.pushNamed(context, '/car_details', arguments: carId);
+              }
+            });
           },
         ),
         const HorizontalSpace(width: 10),
