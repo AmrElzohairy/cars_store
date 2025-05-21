4aad23214ce75dbc9531fce444b824adef217234
amrElzohairy <aelzohairy9@gmail.com>
2025-05-21 20:23:48 +0300

    add search function to repo

--- lib/feature/home/domain/repo/home_repo.dart
+++ lib/feature/home/domain/repo/home_repo.dart
@@ -1,4 +1,5 @@
 import 'package:cars_store/core/errors/failure.dart';
+import 'package:cars_store/feature/home/data/models/search_model/search_model.dart';
 import 'package:cars_store/feature/home/domain/entity/home_featured_list_entity.dart';
 import 'package:cars_store/feature/home/domain/entity/recommended_cars_entity.dart';
 import 'package:dartz/dartz.dart';
@@ -7,4 +8,5 @@ abstract class HomeRepo {
   Future<Either<Failure, List<FeaturedCarsEntity>>> getFeaturedCars();
   Future<Either<Failure, List<RecommendedCarsEntity>>> getRecommendedCars();
   Future<Either<Failure, void>> toggelFavorite({required String carId});
+  Future<Either<Failure,List<SearchModel>>> searchCars({required String query});
 }
